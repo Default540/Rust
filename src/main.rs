923cce4867a57a4a7295ce1b92b049a8a307dc96
Default540 <jv6542436@outlook.com>
2026-06-19 03:06:29 -0300

    update test4

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,21 @@ fn main() {
 
     //println!("{:?}", z); //a variavel z fica invalida
     println!("{:?}", ç);
+
+    let test = "Test".to_string();
+    p(test.clone());
+    p(test); //isso move o valor de heap para a função
+    //p(test); //não pode ser usado pois ja foi movido
+    let test = "Test".to_string();
+    p2(&test);
+    p2(&test);
+
+
+    let mut text = "Test".to_string();
+
+    uppercase(&mut text); //emprestar com a capacidade de mudar o valor, mas não move
+    println!("{text}")
+
 }
 
 fn add_numbers(x: i32, y: i32) -> i32{
@@ -44,4 +59,16 @@ fn add_numbers(x: i32, y: i32) -> i32{
 
 fn funcao_print(){
     println!("Hello, world!");
+}
+
+fn p(txt: String){ //a função pega diretamente a variavel
+    println!("{txt}");
+}
+
+fn p2(txt: &String){ // pega a referencia do valor, não a propria variavel
+    println!("{txt}");
+}
+
+fn uppercase(txt: &mut String){
+    *txt = txt.to_uppercase(); // * é a referencia explicita da variavel, não precisa usar quando ela chama uma função
 }
